578a4f7fca08f902650805150b34704ae49752f6
DennyJuwono <dennyjuw@hotmail.com>
2017-04-18 23:05:44 +1000

    [master] update maf-search-filter

--- src/app/maf-search-filter/maf-search-filter.component.ts
+++ src/app/maf-search-filter/maf-search-filter.component.ts
@@ -13,6 +13,39 @@ export class MafSearchFilterComponent implements OnInit {
   checkbox2 = false;
   checkbox3 = true;
 
+  searchQuery = [
+    { section: 'Gender',
+      options: [
+        {name: 'Male', value: 'gender-male', checked: false},
+        {name: 'Female', value: 'gender-female', checked: false},
+        {name: 'Other', value: 'gender-other', checked: false}
+      ]
+    },
+    { section: 'Background checks',
+      options: [
+        {name: 'Passport', value: 'background-passport', checked: false},
+        {name: 'National Police Criminal Records Check', value: 'background-police', checked: false},
+        {name: 'Working with Children Check (WWCC)', value: 'background-wwcc', checked: false},
+        {name: 'Working with Vulnerable People Check (WWVPC)', value: 'background-wwvpc', checked: false}
+      ]
+    },
+    { section: 'Other preferences',
+      options: [
+        {name: 'LGBTI friendly', value: 'background-passport', checked: false},
+        {name: 'Pet friendly', value: 'background-police', checked: false},
+        {name: 'Non smoker', value: 'background-wwcc', checked: false}
+      ]
+    },
+    { section: 'Qualifications',
+      options: [
+        {name: 'Nursing Services', value: 'background-passport', checked: false},
+        {name: 'Personal Care', value: 'background-police', checked: false},
+        {name: 'Social & Domestic Assitance', value: 'background-wwcc', checked: false}
+      ]
+    }
+  ];
+  
+
   constructor(private _serviceSearchDataService: ServiceSearchDataService) { 
   }
 
@@ -21,6 +54,7 @@ export class MafSearchFilterComponent implements OnInit {
 
   search() {
     console.log('search clicked');
+    console.log(this.searchQuery);
     this._serviceSearchDataService.insertData(this.suburb);
   }
 }
